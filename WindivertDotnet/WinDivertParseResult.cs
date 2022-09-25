c18ce34d427d979e9d169552da26e67da6167ae7
xljiulang <366193849@qq.com>
2022-09-25 13:14:21 +0800

    netstandard2.1

--- WindivertDotnet/WinDivertParseResult.cs
+++ WindivertDotnet/WinDivertParseResult.cs
@@ -5,19 +5,19 @@ namespace WindivertDotnet
 {
     public unsafe class WinDivertParseResult
     {
-        public IPV4Header* IPV4Header { get; init; }
-        public IPV6Header* IPV6Header { get; init; }
-        public ProtocolType Protocol { get; init; }
-        public IcmpV4Header* IcmpV4Header { get; init; }
-        public IcmpV6Header* IcmpV6Header { get; init; }
-        public TcpHeader* TcpHeader { get; init; }
-        public UdpHeader* UdpHeader { get; init; }
-        public byte* Data { get; init; }
-        public int DataLength { get; init; }
+        public IPV4Header* IPV4Header { get; internal set; }
+        public IPV6Header* IPV6Header { get; internal set; }
+        public ProtocolType Protocol { get; internal set; }
+        public IcmpV4Header* IcmpV4Header { get; internal set; }
+        public IcmpV6Header* IcmpV6Header { get; internal set; }
+        public TcpHeader* TcpHeader { get; internal set; }
+        public UdpHeader* UdpHeader { get; internal set; }
+        public byte* Data { get; internal set; }
+        public int DataLength { get; internal set; }
         public Span<byte> DataSpan => new(this.Data, this.DataLength);
 
-        public byte* Next { get; init; }
-        public int NextLength { get; init; }
+        public byte* Next { get; internal set; }
+        public int NextLength { get; internal set; }
         public Span<byte> NextSpan => new(this.Next, this.NextLength);
     }
 }
