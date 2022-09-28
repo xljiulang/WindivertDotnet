fb32a22ea7c391ee3a7e0596aeba0e2d2f1a3b8f
陈国伟 <366193849@qq.com>
2022-09-28 11:32:04 +0800

    xml doc

--- WindivertDotnet/IFilter.cs
+++ WindivertDotnet/IFilter.cs
@@ -73,7 +73,7 @@ namespace WindivertDotnet
         /// tcp属性集
         /// </summary>
         [FilterMember("tcp")]
-        ITcp Tcp { get; } 
+        ITcp Tcp { get; }
 
 
         /// <summary>
@@ -129,14 +129,21 @@ namespace WindivertDotnet
             [FilterMember("inbound")]
             bool Inbound { get; }
 
+            /// <summary>
+            /// 回环IP
+            /// </summary>
             [FilterMember("loopback")]
             bool Loopback { get; }
 
-
+            /// <summary>
+            /// 是否已修改
+            /// </summary>
             [FilterMember("impostor")]
             bool Impostor { get; }
 
-
+            /// <summary>
+            /// 是否分片
+            /// </summary>
             [FilterMember("fragment")]
             bool Fragment { get; }
 
@@ -152,18 +159,34 @@ namespace WindivertDotnet
             [FilterMember("subIfIdx")]
             int SubIfIdx { get; }
 
+            /// <summary>
+            /// 协议类型
+            /// <para>ProtocolType</para>
+            /// </summary>
             [FilterMember("protocol")]
             int Protocol { get; }
 
+            /// <summary>
+            /// 本机IP地址
+            /// </summary>
             [FilterMember("localAddr")]
             string LocalAddr { get; }
 
+            /// <summary>
+            /// 本机端口
+            /// </summary>
             [FilterMember("localPort")]
             int LocalPort { get; }
 
+            /// <summary>
+            /// 远程IP地址
+            /// </summary>
             [FilterMember("remoteAddr")]
             string RemoteAddr { get; }
 
+            /// <summary>
+            /// 远程端口
+            /// </summary>
             [FilterMember("remotePort")]
             int RemotePort { get; }
 
@@ -295,12 +318,18 @@ namespace WindivertDotnet
             [FilterMember]
             int HdrLength { get; }
 
+            /// <summary>
+            /// 版本
+            /// </summary>
             [FilterMember]
             int Version { get; }
 
             [FilterMember]
             int TOS { get; }
 
+            /// <summary>
+            /// 包长度
+            /// </summary>
             [FilterMember]
             int Length { get; }
 
@@ -310,24 +339,43 @@ namespace WindivertDotnet
             [FilterMember]
             int TTL { get; }
 
+            /// <summary>
+            /// 协议
+            /// <para>ProtocolType</para>
+            /// </summary>
             [FilterMember]
             int Protocol { get; }
 
+            /// <summary>
+            /// 检验和
+            /// </summary>
             [FilterMember]
             int Checksum { get; }
 
+            /// <summary>
+            /// 源IP地址
+            /// </summary>
             [FilterMember]
             string SrcAddr { get; }
 
+            /// <summary>
+            /// 远程IP地址
+            /// </summary>
             [FilterMember]
             string DstAddr { get; }
         }
 
         public interface IIPV6
         {
+            /// <summary>
+            /// 版本
+            /// </summary>
             [FilterMember]
             int Version { get; }
 
+            /// <summary>
+            /// 有效负载长度
+            /// </summary>
             [FilterMember]
             int Length { get; }
 
@@ -337,9 +385,15 @@ namespace WindivertDotnet
             [FilterMember]
             int HopLimit { get; }
 
+            /// <summary>
+            /// 源IP地址
+            /// </summary>
             [FilterMember]
             string SrcAddr { get; }
 
+            /// <summary>
+            /// 目的IP地址
+            /// </summary>
             [FilterMember]
             string DstAddr { get; }
         }
@@ -347,15 +401,27 @@ namespace WindivertDotnet
 
         public interface ITransfer
         {
+            /// <summary>
+            /// 校验和
+            /// </summary>
             [FilterMember]
             int Checksum { get; }
 
+            /// <summary>
+            /// 负载数据长度
+            /// </summary>
             [FilterMember]
             int PayloadLength { get; }
 
+            /// <summary>
+            /// 源端口号
+            /// </summary>
             [FilterMember]
             int SrcPort { get; }
 
+            /// <summary>
+            /// 目的端口号
+            /// </summary>
             [FilterMember]
             int DstPort { get; }
         }
@@ -395,6 +461,10 @@ namespace WindivertDotnet
 
         public interface IUdp : ITransfer
         {
+            /// <summary>
+            /// 获取或设置Udp包长度
+            /// <para>含头部的8字节</para>
+            /// </summary>
             [FilterMember]
             int Length { get; }
         }
