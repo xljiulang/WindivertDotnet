534a84bff35de08a221208cad47dbd4924146897
xljiulang <366193849@qq.com>
2022-10-12 21:20:03 +0800

    fix bug

--- WindivertDotnet/WindivertRecvOperation.cs
+++ WindivertDotnet/WindivertRecvOperation.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace WindivertDotnet
 {
@@ -6,6 +7,7 @@ namespace WindivertDotnet
     {
         private readonly WinDivertHandle handle;
         private readonly WinDivertPacket packet;
+        private readonly GCHandle addrLenHandle;
 
         public unsafe WindivertRecvOperation(
             WinDivertHandle handle,
@@ -14,23 +16,26 @@ namespace WindivertDotnet
         {
             this.handle = handle;
             this.packet = packet;
+            this.addrLenHandle = GCHandle.Alloc(sizeof(WinDivertAddress), GCHandleType.Pinned);
         }
 
         protected override unsafe bool IOControl(ref int length, ref WinDivertAddress addr, NativeOverlapped* nativeOverlapped)
         {
-            var addrLength = sizeof(WinDivertAddress);
-            return WinDivertNative.WinDivertRecvEx(this.handle, this.packet, this.packet.Capacity, ref length, 0, ref addr, &addrLength, nativeOverlapped);
+            var pAddrLen = (int*)this.addrLenHandle.AddrOfPinnedObject().ToPointer();
+            return WinDivertNative.WinDivertRecvEx(this.handle, this.packet, this.packet.Capacity, ref length, 0, ref addr, pAddrLen, nativeOverlapped);
         }
 
         protected override void SetResult(int length)
         {
             this.packet.Length = length;
+            this.addrLenHandle.Free();
             base.SetResult(length);
         }
 
         protected override void SetException(int errorCode)
         {
             this.packet.Length = 0;
+            this.addrLenHandle.Free();
             base.SetException(errorCode);
         }
     }
