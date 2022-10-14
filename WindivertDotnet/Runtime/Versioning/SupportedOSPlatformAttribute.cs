6cf04520b30c4d14c625802a981ef4b10dd40c76
xljiulang <366193849@qq.com>
2022-10-14 20:26:19 +0800

    不公开SupportedOSPlatformAttribute

--- WindivertDotnet/Runtime/Versioning/SupportedOSPlatformAttribute.cs
+++ WindivertDotnet/Runtime/Versioning/SupportedOSPlatformAttribute.cs
@@ -2,7 +2,7 @@
 namespace System.Runtime.Versioning
 {
     [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Module | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface, AllowMultiple = true, Inherited = false)]
-    public sealed class SupportedOSPlatformAttribute : Attribute
+    sealed class SupportedOSPlatformAttribute : Attribute
     {
         public string PlatformName { get; }
 
