66b14656e872ba2a5f0c70fcde1295bff3564b70
xljiulang <366193849@qq.com>
2022-10-15 17:55:21 +0800

    add WinDivertNative.Loader

--- WindivertDotnet/Runtime/InteropServices/WinDivertNative.cs
+++ WindivertDotnet/Runtime/InteropServices/WinDivertNative.cs
@@ -1,10 +1,9 @@
-using System.IO;
 using System.Threading;
 using WindivertDotnet;
 
 namespace System.Runtime.InteropServices
 {
-    static unsafe class WinDivertNative
+    static unsafe partial class WinDivertNative
     {
         private const string library = "WinDivert.dll";
 
@@ -115,51 +114,5 @@ namespace System.Runtime.InteropServices
         public static extern bool WinDivertHelperDecrementTTL(
             WinDivertPacket packet,
             int packetLen);
-
-
-        static WinDivertNative()
-        {
-            if (Environment.Is64BitProcess)
-            {
-                LoadResource("WindivertDotnet.v222.x64.WinDivert64.sys");
-                LoadResource("WindivertDotnet.v222.x64.WinDivert.dll", true);
-            }
-            else
-            {
-                LoadResource("WindivertDotnet.v222.x86.WinDivert32.sys");
-                LoadResource("WindivertDotnet.v222.x86.WinDivert64.sys");
-                LoadResource("WindivertDotnet.v222.x86.WinDivert.dll", true);
-            }
-        }
-
-        /// <summary>
-        /// º”‘ÿ◊ ‘¥
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="loadLibray"></param>
-        private static void LoadResource(string name, bool loadLibray = false)
-        {
-            var fileName = Path.GetFileNameWithoutExtension(name).Replace('.', Path.DirectorySeparatorChar) + Path.GetExtension(name);
-            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var destFilePath = Path.Combine(appDataPath, fileName);
-
-            if (File.Exists(destFilePath) == false)
-            {
-                var dir = Path.GetDirectoryName(destFilePath);
-                if (string.IsNullOrEmpty(dir) == false)
-                {
-                    Directory.CreateDirectory(dir);
-                }
-
-                using var rsStream = typeof(WinDivert).Assembly.GetManifestResourceStream(name)!;
-                using var fileStream = File.OpenWrite(destFilePath);
-                rsStream.CopyTo(fileStream);
-            }
-
-            if (loadLibray == true)
-            {
-                Kernel32Native.LoadLibrary(destFilePath);
-            }
-        }
     }
 }
