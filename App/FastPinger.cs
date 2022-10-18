bf793f54bdb4ea67c8d7199e886e77e3982c2898
xljiulang <366193849@qq.com>
2022-10-18 23:27:07 +0800

    IcmpHader头增加id和sequenceNumber; 增加FastPinger示例；

---
+++ App/FastPinger.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using WindivertDotnet;
+
+namespace App
+{
+    class FastPinger : IDisposable
+    {
+        private ushort id = 0;
+        private ushort sequenceNumber = 0;
+        private readonly WinDivert divert;
+
+        public FastPinger()
+        {
+            var filter = Filter.True.And(f => f.IsIcmp);
+            this.divert = new WinDivert(filter, WinDivertLayer.Network);
+        }
+
+        /// <summary>
+        /// Ping所有地址
+        /// 占用两个线程
+        /// </summary>
+        /// <param name="startAddr">开始地址</param>
+        /// <param name="count">IP数量</param>
+        /// <param name="delay">最后一个IP发出ping之后的等待回复时长</param>
+        /// <returns></returns>
+        public Task<IPAddress[]> PingAllAsync(IPAddress startAddr, int count, TimeSpan delay)
+        {
+            var dstAddrs = CreateAddrs(startAddr, count);
+            return this.PingAllAsync(dstAddrs, delay);
+        }
+
+        /// <summary>
+        /// 创建IP列表
+        /// </summary>
+        /// <param name="startAddr"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private static IEnumerable<IPAddress> CreateAddrs(IPAddress startAddr, int count)
+        {
+            var start = BinaryPrimitives.ReadUInt32LittleEndian(startAddr.GetAddressBytes());
+            for (var i = 0; i < count; i++)
+            {
+                var value = BinaryPrimitives.ReverseEndianness((uint)(start + i));
+                yield return new IPAddress(value);
+            }
+        }
+
+        /// <summary>
+        /// Ping所有地址
+        /// 占用两个线程
+        /// </summary>
+        /// <param name="dstAddrs">目标地址</param>
+        /// <param name="delay">最后一个IP发出ping之后的等待回复时长</param>
+        /// <returns></returns>
+        public async Task<IPAddress[]> PingAllAsync(IEnumerable<IPAddress> dstAddrs, TimeSpan delay)
+        {
+            var hashSet = new HashSet<IPAddress>();
+            using var cts = new CancellationTokenSource();
+
+            // 开始监听ping的回复
+            var recvTask = this.RecvReplyAsync(hashSet, cts.Token);
+
+            // 对所有ip发ping
+            await this.SendEchoAsync(dstAddrs);
+
+            // 取消监听
+            cts.CancelAfter(delay);
+
+            try
+            {
+                await recvTask;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            // 清洗数据
+            return hashSet.Intersect(dstAddrs).ToArray();
+        }
+
+
+        /// <summary>
+        /// 监听ping的回复
+        /// </summary>
+        /// <param name="replyHashSet">回复的IP列表</param>
+        /// <param name="cancellationToken">取消令牌</param>
+        /// <returns></returns>
+        private async Task RecvReplyAsync(HashSet<IPAddress> replyHashSet, CancellationToken cancellationToken)
+        {
+            using var packet = new WinDivertPacket();
+            using var addr = new WinDivertAddress();
+
+            while (cancellationToken.IsCancellationRequested == false)
+            {
+                await this.divert.RecvAsync(packet, addr, cancellationToken);
+                if (TryGetReply(packet, out var replyAddr))
+                {
+                    replyHashSet.Add(replyAddr);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 解析出icmp回复信息
+        /// </summary>
+        /// <param name="packet">数据包</param>
+        /// <param name="srcAddr">回复的IP</param>
+        /// <returns></returns>
+        private unsafe static bool TryGetReply(WinDivertPacket packet, [MaybeNullWhen(false)] out IPAddress srcAddr)
+        {
+            var result = packet.GetParseResult();
+            if (result.IcmpV4Header != null && result.IcmpV4Header->Code == 0) // 0是icmp的回复
+            {
+                srcAddr = result.IPV4Header->SrcAddr;
+                return true;
+            }
+
+            srcAddr = null;
+            return false;
+        }
+
+        /// <summary>
+        /// 发送icmp的echo命令
+        /// </summary>
+        /// <param name="dstAddrs"></param>
+        /// <returns></returns>
+        private async Task SendEchoAsync(IEnumerable<IPAddress> dstAddrs)
+        {
+            foreach (var address in dstAddrs)
+            {
+                // 使用router计算将进行通讯的本机地址
+                var router = new WinDivertRouter(address);
+                using var packet = this.CreateEchoPacket(router);
+                using var addr = new WinDivertAddress();
+
+                packet.CalcChecksums(addr);     // 计算checksums
+                packet.CalcLoopbackFlag(addr);  // 计算loopback(实际不算也知道不是Loopback)
+                packet.CalcNetworkIfIdx(addr);  // 计算网卡索引
+                packet.CalcOutboundFlag(addr);  // 计算是否为出口(实际不算也知道是Outbound)
+
+                await this.divert.SendAsync(packet, addr);
+            }
+        }
+
+        /// <summary>
+        /// 创建icmp的echo包
+        /// </summary>
+        /// <param name="router"></param>
+        /// <returns></returns>
+        private unsafe WinDivertPacket CreateEchoPacket(WinDivertRouter router)
+        {
+            // ipv4头
+            var ipHeader = new IPV4Header
+            {
+                TTL = 128,
+                Version = 4,
+                DstAddr = router.DstAddress,
+                SrcAddr = router.SrcAddress,
+                Protocol = ProtocolType.Icmp,
+                HdrLength = (byte)(sizeof(IPV4Header) / 4),
+                Id = this.id++,
+                Length = (ushort)(sizeof(IPV4Header) + sizeof(IcmpV4Header))
+            };
+
+            // icmp头
+            var icmpHeader = new IcmpV4Header
+            {
+                Type = 8, // echo
+                Code = 0,
+                Identifier = ipHeader.Id,
+                SequenceNumber = this.sequenceNumber++,
+            };
+
+            // 复制数据到packet缓冲区
+            var packet = new WinDivertPacket(ipHeader.Length);
+            var ptr = (byte*)packet.DangerousGetHandle().ToPointer();
+            *(IPV4Header*)ptr = ipHeader;
+            *(IcmpV4Header*)(ptr + sizeof(IPV4Header)) = icmpHeader;
+
+            packet.Length = ipHeader.Length;
+            return packet;
+        }
+
+        public void Dispose()
+        {
+            this.divert.Dispose();
+        }
+    }
+}
--- WindivertDotnet/IcmpV6Header.cs
+++ WindivertDotnet/IcmpV6Header.cs
@@ -1,5 +1,6 @@
 ﻿using System.Buffers.Binary;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace WindivertDotnet
 {
@@ -7,19 +8,23 @@ namespace WindivertDotnet
     /// IcmpV6头结构
     /// </summary>
     [DebuggerDisplay("Type = {Type}, Code = {Code}, Size = {sizeof(IcmpV6Header)}")]
+    [StructLayout(LayoutKind.Explicit)]
     public struct IcmpV6Header
     {
         /// <summary>
         /// 获取或设置类型
         /// </summary>
+        [FieldOffset(0)]
         public byte Type;
 
         /// <summary>
         /// 获取或设置代码
         /// </summary>
+        [FieldOffset(1)]
         public byte Code;
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        [FieldOffset(2)]
         private ushort checksum;
 
         /// <summary>
@@ -32,6 +37,7 @@ namespace WindivertDotnet
         }
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        [FieldOffset(4)]
         private uint body;
 
         /// <summary>
@@ -42,6 +48,32 @@ namespace WindivertDotnet
         {
             get => BinaryPrimitives.ReverseEndianness(body);
             set => body = BinaryPrimitives.ReverseEndianness(value);
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        [FieldOffset(4)]
+        private ushort identifier;
+
+        /// <summary>
+        /// 获取或设置id
+        /// </summary>
+        public ushort Identifier
+        {
+            get => BinaryPrimitives.ReverseEndianness(identifier);
+            set => identifier = BinaryPrimitives.ReverseEndianness(value);
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        [FieldOffset(6)]
+        private ushort sequenceNumber;
+
+        /// <summary>
+        /// 获取或设置序列号
+        /// </summary>
+        public ushort SequenceNumber
+        {
+            get => BinaryPrimitives.ReverseEndianness(sequenceNumber);
+            set => sequenceNumber = BinaryPrimitives.ReverseEndianness(value);
         }
     }
 
